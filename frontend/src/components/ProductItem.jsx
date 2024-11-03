5eea2097c4ef34cd560063e38f68c82ddfb1ea79
Dhoritri <ishrak.alam32@gmail.com>
2024-11-04 05:43:01 +0600

    Added Navbar,HeroSection,latest,bestseelers

---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,18 @@
+
+import { ShopContext } from '../context/ShopContext'
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+
+const ProductItem = ({id,image,name,price}) => {
+    const {currency} = useContext(ShopContext);
+  return (
+    <Link className='text-gray-600 cursor-pointer' to={`/product/${id}`}>
+        <div className='overflow-hidden'>
+            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="product image" />
+        </div>
+        <p className='pt-3 pb-1 text-center text-sm text-[#FFFFFF] '>{name}</p>
+        <p className='text-sm text-center font-medium text-[#FFFFFF]'>{currency}{price}</p>
+    </Link>
+  )
+}
+export default ProductItem
--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('profileBtn').addEventListener('click', function() {
-    var div = document.getElementById('loginSignup');
-    div.style.display = div.style.display === 'none' ? 'block' : 'none';
-});
