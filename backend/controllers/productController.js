6af5f3a3b81724c99f6ec4d5723071be17098fee
Dhoritri <ishrak.alam32@gmail.com>
2024-11-24 07:17:34 +0600

    "Updated backend code: modified .env file, added cloudinary and dotenv imports, updated productController.js, modified multer.js, deleted produtModel.js, and updated userModel.js"

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,6 @@
-import productRouter from "../routes/productRoute.js";
+import { v2 as cloudinary } from "cloudinary";
+import { parse } from "dotenv";
+import productModel from "../models/productModel.js";
 //add product
 const addProduct = async (req, res) => {
   try {
@@ -11,29 +13,44 @@ const addProduct = async (req, res) => {
       sizes,
       bestseller,
     } = req.body;
-    const image1 = req.files.image1[0];
-    const image2 = req.files.image2[0];
-    const image3 = req.files.image3[0];
-    const image4 = req.files.image4[0];
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
 
-    console.log(
-      name,
-      description,
-      price,
-      category,
-      subCategory,
-      sizes,
-      bestseller
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
     );
-    console.log(image1, image2, image3, image4);
-    req.json({});
+
+    let imagesUrl = await Promise.all(
+      images.map(async () => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+    const productData = 
+    {name,description,category,price: Number(price),subCategory,bestseller: bestseller === "true" ? true : false,sizes: JSON.parse(sizes),image: imagesUrl,date: Date.now()};
+    console.log(productData);
+    const product = new productModel(productData);
+    await product.save();
+    res.json({ success: true, message: "Product Added" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 //list products
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 //remove products
 const removeProduct = async (req, res) => {};
 
--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,4 +1,4 @@
-import multer from "multer";
+import multer from 'multer';
 const storage = multer.diskStorage({
     filename:function(req,file,callBack){
         callBack(null,file.originalname)
@@ -8,3 +8,4 @@ const storage = multer.diskStorage({
 const upload = multer({storage})
 
 export default upload;
+ 
