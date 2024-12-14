a1a284d485ed9021f06103a0e91780ec5639e5c9
Dhoritri <ishrak.alam32@gmail.com>
2024-12-14 22:50:10 +0600

    `Update Login component to use hardcoded backend URL and update Navbar component to fix import and formatting issues`

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import {assets} from '../assets/assets';
-const Navbar = ({setToken}) => {
+import { assets } from "../assets/assets";
+const Navbar = ({ setToken }) => {
   return (
-    <div className='flex items-center py-2 px-[4%] justify-between '>
-      <img className='w-[max(8%,80px)]' src={assets.logo} alt="" />
-      <button onClick={()=> setToken('')}  className='bg-[#ffffff] hover:bg-[#2c78cf] transition .2s ease-in ease-out text-black px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    <div className="flex items-center py-2 px-[4%] justify-between ">
+      <img className="w-[max(8%,80px)]" src={assets.logo} alt="" />
+      <button
+        onClick={() => setToken("")}
+        className="bg-[#ffffff] hover:bg-[#2c78cf] transition .2s ease-in ease-out text-black px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
